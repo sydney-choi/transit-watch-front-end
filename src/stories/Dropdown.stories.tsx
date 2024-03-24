3e3889f0bd3227be648b1091df71f87ead615182
sydney-choi <soyeong0730@gmail.com>
2024-03-24 23:30:13 +0900

    chore: 불필요한 데이터 삭제

--- src/stories/Dropdown.stories.tsx
+++ src/stories/Dropdown.stories.tsx
@@ -18,16 +18,14 @@ export const Default: Story = {
         name: '경기도인재개발원.연구원.평생교육진흥원.여성가족재단',
         direction: '용산우체국',
         stationNumber: ['00001'],
-        hasBookmark: true,
       },
       {
         stationId: '2',
         name: '역명2',
         direction: '종로2가사거리',
         stationNumber: ['00002', '4213'],
-        hasBookmark: false,
       },
-      { stationId: '3', name: '역명3', direction: '광화문역', stationNumber: ['00003'], hasBookmark: false },
+      { stationId: '3', name: '역명3', direction: '광화문역', stationNumber: ['00003'] },
     ],
   },
 };
