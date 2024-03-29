4a33d5fbbb99155de39e2c764a4c0d108e911b16
sydney-choi <soyeong0730@gmail.com>
2024-03-30 02:33:12 +0900

    chore: 타입을 interface로 통일

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,17 +1,17 @@
-export type Station = {
+export interface Station {
   stationId: string;
   stationName: string;
   nextStationName: string;
   arsId: string[];
-};
+}
 
-export type Bus = {
+export interface Bus {
   busNumber: string;
   direction: string;
   firstArrivalTime: string;
   secondArrivalTime: string;
   firstArrivalBusCrowding: string;
   secondArrivalBusCrowding: string;
-};
+}
 
 export type UIType = 'search' | 'bookmark';
