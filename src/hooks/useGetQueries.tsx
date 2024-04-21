d08d122f81303b8d6f57801fd6c47dc78d5d6bfc
sydney-choi <soyeong0730@gmail.com>
2024-04-21 22:52:20 +0900

    chore: 쿼리 설정을 수정

--- src/hooks/useGetQueries.tsx
+++ src/hooks/useGetQueries.tsx
@@ -1,12 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
 import client from '@/lib/fetcher';
-import { Station, GetStationsNearbyRequest, GetStationsNearbyResponse, GetStationDetailResponse } from '@/types/common';
+import {
+  GetSearchStationsRequest,
+  GetStationsNearbyRequest,
+  GetStationsNearbyResponse,
+  GetStationDetailResponse,
+  GetSearchStationsResponse,
+} from '@/types/common';
 
-export const useSearchStations = (searchText: string = '') =>
-  useQuery<Station[]>({
-    queryKey: ['searchStations', searchText],
-    queryFn: async () => (await client.get(`/stations?q=${searchText}`)).data,
-    enabled: !!searchText.length,
+export const useSearchStations = (request: GetSearchStationsRequest) =>
+  useQuery<GetSearchStationsResponse>({
+    queryKey: ['searchStations', request],
+    queryFn: async () =>
+      (
+        await client.get(
+          `/v1/bus-stops/autocomplete?keyword=${request.searchText}&xLongitude=${request.xlongitude}&yLatitude=${request.ylatitude}`,
+        )
+      ).data,
+    enabled: !!request.searchText.length,
     staleTime: Infinity,
   });
 
@@ -16,7 +27,7 @@ export const useGetStationsNearby = (request: GetStationsNearbyRequest) =>
     queryFn: async () =>
       (
         await client.get(
-          `/v1/bus-stops/near?tmX=${request.xlatitude}&tmY=${request.ylongitude}&radius=${request.radius}`,
+          `/v1/bus-stops/near?tmX=${request.xlongitude}&tmY=${request.ylatitude}&radius=${request.radius}`,
         )
       ).data,
     staleTime: Infinity, // TODO: 임시설정, 배포 전 staleTime 변경하기
