ec67082606a3a29409ff5b97d1b10170e45d6877
김하얀 <kasimir273@gmail.com>
2024-05-12 00:55:49 +0900

    pub> 검색박스

--- src/app/publishing/page.tsx
+++ src/app/publishing/page.tsx
@@ -1,19 +1,21 @@
 import { Suspense } from 'react';
-import { Box, Flex, Button } from '@chakra-ui/react';
+import { Image, Input, Box, Flex, Button } from '@chakra-ui/react';
 // import { Main } from '@/components/main';
 import { MapContainer } from '@/components/map';
 // import { SearchIcon } from '@/components/icons';
 
 const Page = () => (
   <Flex>
-    <Box w="20rem" minW="384px">
+    <Box w="20rem" minW="486px">
       {/* <Main /> */}
       {/* 서치박스 */}
       <div className="search-bar">
-        <div>
-          <Button className="logo">
-            <img src="/images/logo_bus.svg" alt="정류장 혼잡도123" />
-          </Button>
+        <Button className="logo">
+          <Image src="/images/logo_bus.svg" alt="정류장 혼잡도" />
+        </Button>
+        <div className="search-ipt">
+          <Input type="text" placeholder="정류장 검색" className="text" />
+          <Button className="btn-search">검색</Button>
         </div>
       </div>
       {/* 서치박스 끝 */}
